33b9abfc809faa264bd74b1760a0ce8c1cd95e3a
piercirocaliandro <56635040+piercirocaliandro@users.noreply.github.com>
2020-03-09 20:18:16 +0100

    #7

--- src/main/java/logic/dao/TravelDao.java
+++ src/main/java/logic/dao/TravelDao.java
@@ -14,30 +14,34 @@ import java.sql.Date;
 
 public class TravelDao extends GeneralConnection{
 	
-	public TravelDao() {
-		getConnection();
-	}
-	
 	public List<TicketModel> retriveAvailableTickets(UserTravelBean travBean) throws SQLException {
+		if(dbConn == null) {
+			getConnection();
+		}
 		List<TicketModel> tickets = new ArrayList<>();
-		ResultSet rs = null;
 		try(PreparedStatement prep = dbConn.getConnection().prepareStatement("SELECT * FROM Tickets WHERE (depCity=? and arrCity=? and dateOfDep=? and dateOfArr=?)")) {
 			prep.setString(1, travBean.getCityOfDep());
 			prep.setString(2, travBean.getCityOfArr());
 			prep.setString(3, Date.valueOf(travBean.getFirstDay()).toString());
 			prep.setString(4, Date.valueOf(travBean.getLastDay()).toString());
-			rs = prep.executeQuery();
-			while(rs.next()) {
-				TicketModel tick= new TicketModel();
-				tick.setAll(rs.getString(2), rs.getString(3), rs.getDate(4).toLocalDate(), rs.getDate(5).toLocalDate(), rs.getFloat(6));
-				tickets.add(tick);
-			}
+			ticketQuery(tickets, prep);
 		}catch(SQLException e) {
 			logger.log(Level.SEVERE, "Error while retriving tickets\n",e);
 		}
-		finally {
-			rs.close();
-		}
 		return tickets;
+	}
+	
+	/* the executeQuery should be in a function, maybe in the generalConnection Dao*/
+	
+	public void ticketQuery(List<TicketModel> list, PreparedStatement prep) {
+		try (ResultSet rs = prep.executeQuery()){
+			while(rs.next()) {
+				TicketModel tick= new TicketModel();
+				tick.setAll(rs.getString(2), rs.getString(3), rs.getDate(4).toLocalDate(), rs.getDate(5).toLocalDate(), rs.getFloat(6));
+				list.add(tick);
+			}
+		}catch(SQLException e) {
+			logger.log(Level.SEVERE, "RseultSet null \n",e);
+		}
 	}
 }
