61ade4ea9fbf25c3c86966c11e929be7b89330ec
Gian Marco Falcone <56635080+ShockGiammy@users.noreply.github.com>
2020-06-11 11:32:07 +0200

    #12

--- src/main/java/logic/model/GroupMessage.java
+++ src/main/java/logic/model/GroupMessage.java
@@ -12,42 +12,52 @@ public class GroupMessage implements Message {
     private String group;
     private ChatType chatType = ChatType.GROUP;
 
+    @Override
     public String getName() {
         return myName;
     }
 
+    @Override
     public void setName(String name) {
         this.myName = name;
     }
 
+    @Override
     public String getMsg() {
         return groupMsg;
     }
 
+    @Override
     public void setMsg(String msg) {
         this.groupMsg = msg;
     }
 
+    @Override
     public MessageType getType() {
         return type;
     }
 
+    @Override
     public void setType(MessageType type) {
         this.type = type;
     }
 
+    @Override
 	public String getGroupOrReceiver() {
 		return group;
 	}
 
+    @Override
 	public void setGroupOrReceiver(String group) {
 		this.group = group;
 	}
 
+    @Override
 	public ChatType getChatType() {
 		return chatType;
 	}
 	
+    @Override
 	public void save() {
 		ChatDao dao = new ChatDao();
 		dao.saveMessage(this);
--- src/main/java/logic/model/PrivateMessage.java
+++ src/main/java/logic/model/PrivateMessage.java
@@ -12,42 +12,52 @@ public class PrivateMessage implements Message {
     private String receiver;
     private ChatType chatType = ChatType.PRIVATE;
 
+    @Override
     public String getName() {
         return name;
     }
 
+    @Override
     public void setName(String name) {
         this.name = name;
     }
 
+    @Override
     public String getMsg() {
         return privateMsg;
     }
 
+    @Override
     public void setMsg(String msg) {
         this.privateMsg = msg;
     }
 
+    @Override
     public MessageType getType() {
         return type;
     }
-
+    
+    @Override
     public void setType(MessageType type) {
         this.type = type;
     }
 
+    @Override
 	public String getGroupOrReceiver() {
 		return receiver;
 	}
 
+    @Override
 	public void setGroupOrReceiver(String user) {
 		this.receiver = user;
 	}
 
+    @Override
 	public ChatType getChatType() {
 		return chatType;
 	}
 	
+    @Override
 	public void save() {
 		ChatDao dao = new ChatDao();
 		dao.saveMessage(this);
